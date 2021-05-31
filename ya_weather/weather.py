e99e144a0a68618a6af08a6406ca1386e3d963c6
Slava Shishkin <slavashishkin@gmail.com>
2021-05-31 11:30:07 +0300

    Minor experimental changes in mine logic

--- ya_weather/weather.py
+++ ya_weather/weather.py
@@ -1,11 +1,11 @@
-from flask import (Blueprint, flash, g, redirect, render_template, request, url_for, Response)
+from flask import (Blueprint, g, redirect, render_template, Response)
 
 from ya_weather.auth import login_required
+from ya_weather.csv_maker import csv_maker
 from ya_weather.db import get_db
 from ya_weather.req_forecast import dump_request
 
 bp = Blueprint('weather', __name__)
-
 
 
 @bp.route('/')
@@ -19,32 +19,28 @@ def index():
     return render_template('index.html', posts=posts)
 
 
-@bp.route('/download', methods=('GET', 'POST'))
-def download_csv():
+@bp.route('/<int:id>/download', methods=('GET', 'POST'))
+def download_csv(id):
     db = get_db()
-    posts = db.execute(
+    post = db.execute(
         'SELECT w.id, created, cit_1, cit_2, cit_3, cit_4, cit_5, temp_1, temp_2, temp_3, temp_4, temp_5, f_l_1, f_l_2, f_l_3, f_l_4, f_l_5, con_1, con_2, con_3, con_4, con_5, author_id, username'
         ' FROM weather w JOIN user u ON w.author_id = u.id'
         ' ORDER BY created DESC'
-    ).fetchall()
-    created = posts[0]['created']
-    city = posts[0]['city']
-    temperature = posts[0]['temperature']
-    con_ = posts[0]['con_']
+    ).fetchall()[id-1]
 
-    csv = f'created: {created}\ncity: {city}\ntemperature: {temperature}\ncon_: {con_}'
+    csv = csv_maker(post)
     return Response(
         csv,
         mimetype="text/csv",
         headers={"Content-disposition":
-                 "attachment; filename=ya_weather_data.csv"})
+                     "attachment; filename=ya_weather_data.csv"})
 
 
 @bp.route('/create', methods=('GET', 'POST'))
 @login_required
 def create():
     dump = dump_request()
-    print(dump)
+
     cit_1 = dump[0]['city']
     cit_2 = dump[1]['city']
     cit_3 = dump[2]['city']
@@ -70,9 +66,9 @@ def create():
     db.execute(
         'INSERT INTO weather (cit_1, cit_2, cit_3, cit_4, cit_5, temp_1, temp_2, temp_3, temp_4, temp_5, f_l_1, f_l_2, f_l_3, f_l_4, f_l_5, con_1, con_2, con_3, con_4, con_5, author_id)'
         ' VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)',
-        (cit_1, cit_2, cit_3, cit_4, cit_5, temp_1, temp_2, temp_3, temp_4, temp_5, f_l_1, f_l_2, f_l_3, f_l_4, f_l_5, con_1, con_2, con_3, con_4, con_5, g.user['id'])
+        (cit_1, cit_2, cit_3, cit_4, cit_5, temp_1, temp_2, temp_3, temp_4, temp_5, f_l_1, f_l_2, f_l_3, f_l_4, f_l_5,
+         con_1, con_2, con_3, con_4, con_5, g.user['id'])
     )
     db.commit()
 
-    # download_csv()
     return redirect('/')
